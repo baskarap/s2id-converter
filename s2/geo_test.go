cccde58ee1ee3b4a750676337b892239984ea8f1
baskara <baskara@go-jek.com>
2019-03-08 17:48:55 +0700

    Change package name from s2 to s2_test for geo_test.go

--- s2/geo_test.go
+++ s2/geo_test.go
@@ -1,20 +1,21 @@
-package s2
+package s2_test
 
 import (
+	"github.com/baskarap/s2id-converter/s2"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestNewGeo_ReturnsNonNilGeoObject(t *testing.T) {
-	geo := NewGeo(1)
+	geo := s2.NewGeo(1)
 
 	assert.NotNil(t, geo)
-	assert.IsType(t, Geo{}, *geo)
+	assert.IsType(t, s2.Geo{}, *geo)
 }
 
 func TestGetS2ID_ReturnsError_WhenInvalidLatlongIsGiven(t *testing.T) {
 	invalidLatLng := 190.0
-	geo := NewGeo(1)
+	geo := s2.NewGeo(1)
 	s2id, err := geo.GetS2ID(invalidLatLng, invalidLatLng)
 
 	assert.Empty(t, s2id)
