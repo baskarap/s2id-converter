deef457fb9b70d7f3dbdf5d79184c194629b03b9
baskara <baskara@go-jek.com>
2019-03-08 18:53:23 +0700

    Add success scenario for getting S2ID with valid latlng and level

--- s2/geo.go
+++ s2/geo.go
@@ -2,6 +2,7 @@ package s2
 
 import (
 	"errors"
+	"fmt"
 	"github.com/golang/geo/s2"
 )
 
@@ -20,7 +21,19 @@ func (g *Geo) GetS2ID(lat float64, lng float64) (string, error) {
 		return "", errors.New(ErrInvalidLatlng)
 	}
 
-	return "", errors.New(ErrInvalidLevel)
+	if !isValidLevel(g.level) {
+		return "", errors.New(ErrInvalidLevel)
+	}
+
+	cell := s2.CellIDFromLatLng(latLng).Parent(g.level)
+	return fmt.Sprintf("%d", cell), nil
+}
+
+func isValidLevel(level int) bool {
+	if level <= 30 && level > -1 {
+		return true
+	}
+	return false
 }
 
 func NewGeo(level int) *Geo {
--- s2/geo_test.go
+++ s2/geo_test.go
@@ -6,6 +6,11 @@ import (
 	"testing"
 )
 
+const (
+	ValidLat = -6.244378
+	ValidLng = 106.798477
+)
+
 func TestNewGeo_ReturnsNonNilGeoObject(t *testing.T) {
 	geo := s2.NewGeo(1)
 
@@ -24,8 +29,16 @@ func TestGetS2ID_ReturnsError_WhenInvalidLatLngIsGiven(t *testing.T) {
 
 func TestGetS2ID_ReturnsError_WhenInvalidLevelIsBeingSet(t *testing.T) {
 	geo := s2.NewGeo(99)
-	s2id, err := geo.GetS2ID(-6.233728, 106.790695)
+	s2id, err := geo.GetS2ID(ValidLat, ValidLng)
 
 	assert.Empty(t, s2id)
 	assert.Equal(t, s2.ErrInvalidLevel, err.Error())
+}
+
+func TestGetS2ID_ReturnsS2ID_WhenValidLatLngAndLevelAreGiven(t *testing.T) {
+	geo := s2.NewGeo(13)
+	s2id, err := geo.GetS2ID(ValidLat, ValidLng)
+
+	assert.Equal(t, "3344469644458065920", s2id)
+	assert.Nil(t, err)
 }
