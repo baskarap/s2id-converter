594666e93238140801853c543a80b9bfa3fe0a5d
baskara <baskara@go-jek.com>
2019-03-08 17:46:02 +0700

    Add GetS2ID implementation, it always returns invalid-lat-lng error and empty string

--- s2/geo.go
+++ s2/geo.go
@@ -1,7 +1,17 @@
 package s2
 
+import (
+	"errors"
+)
+
+const ErrInvalidLatlng = "Invalid LatLng being sent"
+
 type Geo struct {
 	level int
+}
+
+func (g *Geo) GetS2ID(lat float64, lng float64) (string, error) {
+	return "", errors.New(ErrInvalidLatlng)
 }
 
 func NewGeo(level int) *Geo {
--- s2/geo_test.go
+++ s2/geo_test.go
@@ -11,3 +11,12 @@ func TestNewGeo_ReturnsNonNilGeoObject(t *testing.T) {
 	assert.NotNil(t, geo)
 	assert.IsType(t, Geo{}, *geo)
 }
+
+func TestGetS2ID_ReturnsError_WhenInvalidLatlongIsGiven(t *testing.T) {
+	invalidLatLng := 190.0
+	geo := NewGeo(1)
+	s2id, err := geo.GetS2ID(invalidLatLng, invalidLatLng)
+
+	assert.Empty(t, s2id)
+	assert.NotNil(t, err)
+}
